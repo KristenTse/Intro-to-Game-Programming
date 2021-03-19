2381a4befab7f9d60e91d8ddab4b17c9be1d604d
KristenTse <49358081+KristenTse@users.noreply.github.com>
2021-03-19 02:30:05 -0400

    added sound to p2

--- P2/SDLProject/main.cpp
+++ P2/SDLProject/main.cpp
@@ -5,6 +5,7 @@
 #endif
 
 #define GL_GLEXT_PROTOTYPES 1
+#include <SDL_mixer.h>
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include "glm/mat4x4.hpp"
@@ -48,6 +49,10 @@ float ball_side = 1.0f * ball_scale.x;
 float uWidth = 5.0f;
 float uHeight = 3.75f;
 
+// Audio
+Mix_Music* music;
+Mix_Chunk* bounce;
+
 
 GLuint LoadTexture(const char* filePath) {
     int w, h, n;
@@ -72,7 +77,7 @@ GLuint LoadTexture(const char* filePath) {
 
 
 void Initialize() {
-    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
     displayWindow = SDL_CreateWindow("Pong!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
     SDL_GL_MakeCurrent(displayWindow, context);
@@ -84,6 +89,13 @@ void Initialize() {
     glViewport(0, 0, 640, 480);
 
     program.Load("shaders/vertex.glsl", "shaders/fragment.glsl");
+
+    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 4096);
+    music = Mix_LoadMUS("dooblydoo.mp3");
+    Mix_PlayMusic(music, -1);
+    Mix_VolumeMusic(MIX_MAX_VOLUME / 5);
+
+    bounce = Mix_LoadWAV("bounce.wav");
 
     viewMatrix = glm::mat4(1.0f);
     pLeftMatrix = glm::mat4(1.0f);
@@ -190,9 +202,11 @@ bool isColliding(glm::vec3 pPos, float &colOffset) {
 void handleHitPaddle() {
     float colOffset;
     if (isColliding(pLeft_position, colOffset)) {
+        Mix_PlayChannel(-1, bounce, 0);
         ball_position.x = pLeft_position.x + p_width;
     }
     else if (isColliding(pRight_position, colOffset)) {
+        Mix_PlayChannel(-1, bounce, 0);
         ball_position.x = pRight_position.x - p_width;
     }
     else {
